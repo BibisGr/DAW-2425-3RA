3045bac7e978da4632a96c89d2596a3cabeba477
bibis <bertha@gmail.com>
2025-03-31 23:18:09 +0200

    preparación clase 5

--- Angular-IV/VeladaAngular/src/app/componentes/page-not-found/page-not-found.component.ts
+++ Angular-IV/VeladaAngular/src/app/componentes/page-not-found/page-not-found.component.ts
@@ -3,7 +3,7 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-page-not-found',
-  imports: [RouterLink, RouterOutlet],
+  imports: [RouterLink],
   templateUrl: './page-not-found.component.html',
   styleUrl: './page-not-found.component.css'
 })

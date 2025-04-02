667297d49b9e6ee5f012e827ad14224602cf9612
bibis <bertha@gmail.com>
2025-04-02 10:02:51 +0200

    modificaciones clase 4 parte 1

--- Angular-IV/VeladaAngular/src/app/componentes/jugadores/jugadores.component.ts
+++ Angular-IV/VeladaAngular/src/app/componentes/jugadores/jugadores.component.ts
@@ -7,5 +7,39 @@ import { Component } from '@angular/core';
   styleUrl: './jugadores.component.css'
 })
 export class JugadoresComponent {
-
+  superheroes = {
+    SquadName: 'Los dawengadores',
+    baseSecreta: 'Aula 1',
+    city: 'Valencia City',
+    miembros:[
+      {
+        alias: 'Ironman',
+        identidadSecreta: 'Tony Stark',
+        heroeactivo: true,
+        superpoderes: [
+          'Volar',
+          'Disparar rayos',
+          'Inteligencia superior']
+      },
+      {
+        alias: 'Hulk',
+        identidadSecreta: 'Bruce Banner',
+        heroeactivo: true,
+        superpoderes: [
+          'Fuerza sobrehumana',
+          'Resistencia',
+          'Regeneración']
+      },
+      {
+        alias: 'Thor',
+        identidadSecreta: 'Thor Odinson',
+        heroeactivo: true,
+        superpoderes: [
+          'Controlar el trueno',
+          'Volar',
+          'Fuerza sobrehumana']
+      },
+      
+    ]
+  }
 }
